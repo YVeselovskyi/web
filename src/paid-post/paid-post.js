6b71453490ea7e3931975e8641c7cd1088c39c5b
YVeselovskyi <19dynamokyiv94@gmail.com>
2023-02-09 15:09:20 +0200

    ADDED: Log test

--- src/paid-post/paid-post.js
+++ src/paid-post/paid-post.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function MyComponent({ title, description, onClick }) {
+  const onClickHandler = () => {
+    console.log(window, window.ethereum);
+    return onClick && onClick()
+  }
+
   return (
-    <div className="paid-post" onClick={onClick}>
+    <div className="paid-post" onClick={onClickHandler}>
       <h2>{title}</h2>
       <p>{description}</p>
     </div>
